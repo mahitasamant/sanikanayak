a432ef76c930b887859854dd0d9f0ea836ee5235
mahitasamant <64544382+mahitasamant@users.noreply.github.com>
2020-10-14 13:36:15 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+     $('.owl-carousel').owlCarousel({
+      loop: true,
+      autoplay: false,
+      autoplayTimeout: 3000,
+      responsive: responsive}
+     );
+    });
